200fca232448be03b35cd7d833780a02d589f853
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-15 11:17:15 +0100

    Narrowed down the way it works

--- test/karma.tests.js
+++
@@ -1,7 +0,0 @@
-const integrationContext
-  = require.context('./integration', true, /\.(js)$/);
-integrationContext.keys().forEach(integrationContext);
-
-const unitContext
-  = require.context('../src/', true, /\.spec\.(js)$/);
-unitContext.keys().forEach(unitContext);
--- test/register-babel.js
+++
@@ -1,6 +0,0 @@
-require('babel-core/register')({
-  ignore: /node_modules/,
-  plugins: ["transform-react-jsx", "jsx-control-statements"],
-  cache: false,
-  presets: ["babel-preset-react"]
-})
