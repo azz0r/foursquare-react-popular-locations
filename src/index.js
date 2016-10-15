9644820d32239cbfe2ece074a1a52e24ec116f4b
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-15 08:07:17 +0100

    Hot reloading

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app/app'
-const rootEl = document.getElementById('root')
 
-ReactDOM.render(
-  <App />,
-  rootEl
-)
+const rootEl = document.getElementById('root');
+const HmrContainer = (process.env.NODE_ENV === 'development')
+  ? require('react-hot-loader').AppContainer
+  : ({ children }) => (children);
 
-if (module.hot) {
-  module.hot.accept('./components/app/app', () => {
-    const NextApp = require('./components/app/app').default;
-    ReactDOM.render(
-      <NextApp />,
-      rootEl
-    )
-  })
+export const Container = (
+  <HmrContainer>
+    <App />
+  </HmrContainer>
+);
+
+try {
+  ReactDOM.render(Container, rootEl);
+  if (module.hot) {
+    module.hot.accept('./components/app/app', () => {
+      const NextApp = require('./components/app/app').default;
+      ReactDOM.render(
+        <HmrContainer>
+          <NextApp />
+        </HmrContainer>,
+        rootEl
+      );
+    });
+  }
+} catch (err) {
+  console.log('Render error', err);
 }
