a12e25bb4014bb999469af5005ecc23389d8f6b2
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-15 05:28:47 +0100

    Added hot reload

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/app/app'
+const rootEl = document.getElementById('root');
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  rootEl
 )
+
+if (module.hot) {
+  module.hot.accept('./components/app/app', () => {
+    const NextApp = require('./components/app/app').default;
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    );
+  });
+}
